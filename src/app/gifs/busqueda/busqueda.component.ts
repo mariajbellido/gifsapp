1986e5e9f8e808876390186fc6672bc90a3bfbf8
mariajbellido <mariajbellido@gmail.com>
2021-10-20 16:44:43 +0200

    Componentes para búsqueda y resultado

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+ 
+})
+export class BusquedaComponent {
+
+  
+
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 @NgModule({
 
   declarations: [
+    GifsPageComponent,
+    BusquedaComponent,
+    ResultadosComponent
+  ],
+  exports: [
     GifsPageComponent
   ],
   imports: [
     CommonModule
-  ],
-  exports: [
-    GifsPageComponent
   ]
+  
 
 })
 export class GifsModule { }
