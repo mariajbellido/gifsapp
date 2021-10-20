09cbed3d5f91ca4627331f86ea6d8c5f11427fa0
mariajbellido <mariajbellido@gmail.com>
2021-10-20 15:41:46 +0200

    Creando gifs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, SharedModule ],
+  imports:      [ BrowserModule, FormsModule, SharedModule, GifsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
