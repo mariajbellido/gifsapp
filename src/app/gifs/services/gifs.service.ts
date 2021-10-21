1c9b1166b517cecc7c9e1e318c610427cc51e00d
mariajbellido <mariajbellido@gmail.com>
2021-10-21 09:22:54 +0200

    Controlando historial de búsquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,11 +9,20 @@ export class gifsService {
   private _historial: string[] = [];
 
   get historial() {
+    
     return [...this._historial];
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift( query );
+  buscarGifs(query: string = '') {
+   
+    query = query.trim().toLocaleLowerCase();
+
+    if (!this._historial.includes( query )) {
+      this._historial.unshift( query ); // Añadiendo nuevos elementos al principio del array (si NO existen en el historial)
+      this._historial = this._historial.splice(0,10); // Limitando a 10 elementos nuestro sidebar
+    }
+
+    
     console.log(this._historial)
   }
 
